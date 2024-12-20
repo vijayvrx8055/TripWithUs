4fcff57231c3758db497fbd896f124eacdf639c3
vijayvrx8055 <vijay.kumar.vrx@gmail.com>
2024-12-20 19:23:02 +0530

    hotels.json added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { resolve } = require('path');
 let cors = require('cors');
+let hotels = require('./hotels');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('static'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
