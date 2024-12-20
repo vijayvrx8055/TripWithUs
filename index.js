33f829995c9c6ff03d775d56cd7302deb69c1aa8
vijayvrx8055 <vijay.kumar.vrx@gmail.com>
2024-12-20 19:39:58 +0530

    hotels/sort/pricing flow added

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+//functions------------------------------------------
+function sortPricingHighToLow(hotel1,hotel2){
+  return hotel2.pricing - hotel1.pricing;
+}
+function sortPricingLowToHigh(hotel1,hotel2){
+  return hotel1.pricing - hotel2.pricing;
+}
+//--------------------------------------------------------
+app.get('/hotels/sort/pricing',(req,res)=>{
+  let pricing = req.query.pricing;
+  let hotelsCopy = hotels.slice();
+  if(pricing==="low-to-high"){
+    hotelsCopy.sort(sortPricingLowToHigh);
+  }else if(pricing==="high-to-low"){
+    hotelsCopy.sort(sortPricingHighToLow);
+  }
+  res.json(hotelsCopy);
+  
+});
 
 
 app.listen(port, () => {
