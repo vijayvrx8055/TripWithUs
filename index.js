cb819d955104bd868a50baf9b39a846d751d9d9b
vijayvrx8055 <vijay.kumar.vrx@gmail.com>
2024-12-20 20:28:18 +0530

    rearranged

--- index.js
+++ index.js
@@ -13,13 +13,25 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-//functions-===================================
+//=------------------------------------------------
 function sortPricingHighToLow(hotel1,hotel2){
   return hotel2.price - hotel1.price;
 }
 function sortPricingLowToHigh(hotel1,hotel2){
   return hotel1.price - hotel2.price;
 }
+app.get('/hotels/sort/pricing',(req,res)=>{
+  let pricing = req.query.pricing;
+  let hotelsCopy = hotels.slice();
+  if(pricing==='low-to-high'){
+    hotelsCopy.sort(sortPricingLowToHigh);
+  }else if(pricing==='high-to-low'){
+    hotelsCopy.sort(sortPricingHighToLow);
+  }
+  res.json(hotelsCopy);
+});
+
+
 //-----------------------------------------
 function sortRatingHighToLow(hotel1,hotel2){
   return hotel2.rating - hotel1.rating;
@@ -27,20 +39,6 @@ function sortRatingHighToLow(hotel1,hotel2){
 function sortRatingLowToHigh(hotel1,hotel2){
   return hotel1.rating - hotel2.rating;
 }
-//-=============================================
-app.get('/hotels/sort/pricing',(req,res)=>{
-  let pricing = req.query.pricing;
-  let hotelsCopy = hotels.slice();
-  if(pricing==='low-to-high'){
-    hotelsCopy.sort(sortPricingLowToHigh);
-    res.json(hotelsCopy);
-  }else if(pricing==='high-to-low'){
-    hotelsCopy.sort(sortPricingHighToLow);
-    res.json(hotelsCopy);
-  }
-  
-});
-
 app.get('/hotels/sort/rating',(req,res)=>{
   let rating = req.query.rating ;
   let hotelsCopy = hotels.slice();
@@ -51,6 +49,29 @@ app.get('/hotels/sort/rating',(req,res)=>{
   }
   res.json(hotelsCopy);
 });
+//-----------------------------------------
+function sortReviewsMostToLeast(hotel1,hotel2){
+  return hotel2.reviews - hotel1.reviews;
+}
+function sortReviewsLeastToMost(hotel1,hotel2){
+  return hotel1.reviews - hotel2.reviews;
+}
+app.get('/hotels/sort/reviews',(req,res)=>{
+  let reviews  = req.query.reviews  ;
+  let hotelsCopy = hotels.slice();
+  if(reviews ==='least-to-most'){
+    hotelsCopy.sort(sortReviewsLeastToMost);
+  }else if(reviews ==='most-to-least'){
+    hotelsCopy.sort(sortReviewsMostToLeast);
+  }
+  res.json(hotelsCopy);
+});
+
+//-=============================================
+
+
+
+
 
 
 app.listen(port, () => {
